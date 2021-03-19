8800779f25eb9b953176eb884793c5e139191ba1
davidvanr21 <50579850+davidvanr21@users.noreply.github.com>
2021-03-19 10:12:58 +0100

    Final push

--- closed-captions/captions.js
+++ closed-captions/captions.js
@@ -3,7 +3,7 @@ SCENE 1
 ********************************************** */
 
 captions[0] = ['3.8','8.1'];
-captions[1] = ['8.1','9'];
+captions[1] = ['8.1','10'];
 captions[2] = ['10.8','12.5'];
 	//And blood-black nothingness began to spin
 captions[3] = ['12.5','15.6'];
